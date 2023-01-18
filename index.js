22536bf5a5fc97217b90b8a38b3ab2bcc61fcde4
sfera1111111 <samarbekjymakadurov@gmail.com>
2023-01-18 15:22:04 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let myName = "Samarbek";
+
+console.log(myName);
+console.log("My name's length " + myName.length);
+console.log(myName.toLowerCase());
+console.log(myName.toUpperCase());
+
+let pi = 3.141493;
+console.log(pi);
+console.log(pi.toFixed(2));
+console.log(pi.toFixed(0));
+let example1 = 2 + 2;
+let example2 = 2 - 2;
+let example3 = 2 * 2;
+let example4 = 2 / 2;
+let example5 = 2 ** 4;//степень (кватрат)
+let example6 = 81 ** (1/3);//кубический корень
+let example7 = 5 % 2;
+let example8 = "Hello " + "World " + '7';
+
+let example9 = 4 + 10 + 12;
+let example10 = 1 + 5 * 3;
+let example11 = 18 * (1 - 1);
+
+let example12 = 5 + '3';//не использивать
+let example13 = '5' + 3;
+let example14 = '5' * 3;//15
+let example15 = 'a' * 3;//NaN
+
+let example16 = -15;
+let example17 = +15
+
+console.log(example17);//остаток
